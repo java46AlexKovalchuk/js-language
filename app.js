f0e290e4ee0f6e03264de2aa3973ce8b774e0ef9
AlexKovalchuk <kovalchuk111@gmail.com>
2022-01-12 15:31:47 +0200

    hw-10_1

--- app.js
+++ app.js
@@ -1,33 +1,57 @@
-console.log("Hello World");
-//output for var: 3 3 3 
-for (let i = 0; i < 3; i++){
-   setTimeout (function() {
-        console.log(i)});
-    }
 
-   // for (let i = 0; i < 3; i++);{
-    
-     //   console.log(i);
-    //}   
-    //console.log(i);
-    const fun = function(a, b) {
-        return a + b;
-    }
-    let a = 5;
-    //console.log(a ** 2);
-    //console.log(fun(10,20));
-   // console.log(fun ** 2);
-   console.log("12" + 12, '"12" + 12');
-   console.log("12" - 12);
-   console.log("ab" - 12);
-   //sometimes there is a need to explicit conversion from string to the number
-   //"+" unary is the simplest way for the explicit conversion of a string to numb
-    console.log('+"12" + 12 = ${+"12" + 12}');
-    //home work#10
-    //1.using only two letters "a", "s" print out word "ananas" case insensitive
-    //2.write function calculate that can perform any arithmetic operations on two numbers
-    //3.write any exapmle for running the following expression fun(5)(10, 3)
-    console.log(+"12" + 12);
-    console.log('12 + 12'); 
-    console.log(`${+"12" + 12}`);
+//1. Typing "ANANAS"
+
+const ananasObject = {letter1: "a", syllable1: `${"a" - 1}`, syllable2: "as"};
+console.log(ananasObject.letter1, ananasObject.syllable1, ananasObject.syllable2);
+
+
+//2. Calculator
+let num1 = 100;
+let signAct = "/";
+let num2 = 5;
+console.log(calculator(num1, signAct, num2));
+function calculator (num1, signAct, num2) {
+  switch(signAct){
+    case signAct = "+":
+      return num1 + num2;
+      break;
+    case signAct = "-":
+      return num1 - num2;
+      break;
+    case signAct = "*":
+      return num1 * num2;
+      break;
+    case signAct = "/":
+      if (num2 != 0)
+        {
+        return num1 / num2;
+        }
+      else
+        {
+        console.log('ERROR: division by zero!');  
+        }
+      break;
+    default: 
+      console.log('ERROR: check the input of operation symbol');
+  }
+}
+
+
+//3. expression of type fun(10)(3,5)
+  //3.1
+const res = fun(5)(10, 3);
+  function fun(a)  {
+  return function (b,c) {
+         return a + (b*c)
+         }
+      };
+console.log(res);
+   
+  //3.2
+function func(a){
+  return function (b,c) {
+  return a * (b ** c)
+  }
+};
+console.log( func(5)(10,3));
 
