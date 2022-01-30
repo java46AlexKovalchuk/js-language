8176a14172e9280611b1e2a8c563c8101b854ddb
AlexKovalchuk <kovalchuk111@gmail.com>
2022-01-31 01:07:48 +0200

    commit_hw-14_with part 2

--- app.js
+++ app.js
@@ -46,26 +46,26 @@ console.log(count);
 // console.log(arOrd);
 
 // const test = ['hgf', 'fdgfd', 'jhfhggd', 'dsads'];
-function displayOccurrences(array) {
-  const res = {};
-  for (let i = 0; i < array.length; i++) {
-          if (res[array[i]] === undefined) {
-                  //string as content of array[i] occures first time
-                  res[array[i]] = 1;
-          } else {
-                  res[array[i]] = res[array[i]] + 1;
-          }
-  }
-  //console.log(res) //-> intermediate debug log
-  Object.entries(res).sort((e1, e2) => {
-          const res = e2[1] - e1[1];
-          return res === 0 ? e1[0].localeCompare(e2[0]) :  res;
-  }).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
+// function displayOccurrences(array) {
+//   const res = {};
+//   for (let i = 0; i < array.length; i++) {
+//           if (res[array[i]] === undefined) {
+//                   //string as content of array[i] occures first time
+//                   res[array[i]] = 1;
+//           } else {
+//                   res[array[i]] = res[array[i]] + 1;
+//           }
+//   }
+//   //console.log(res) //-> intermediate debug log
+//   Object.entries(res).sort((e1, e2) => {
+//           const res = e2[1] - e1[1];
+//           return res === 0 ? e1[0].localeCompare(e2[0]) :  res;
+//   }).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
 
 
-}
-const ar =  ["bc", "lmn", "d", "d", "lmn", "a", "lmn", "a"];
-displayOccurrences(ar);
+// }
+// const ar =  ["bc", "lmn", "d", "d", "lmn", "a", "lmn", "a"];
+// displayOccurrences(ar);
 
 
 
@@ -81,17 +81,54 @@ displayOccurrences(ar);
 
 function dispStat (ar){ 
   
-  const objArray = ar.reduce((total, elem) => {total[elem] = (total[elem] || 0) + 1; 
-  return total; }, {});
+  const objArray = objOccurances(ar);
+  //ar.reduce((total, elem) => {total[elem] = (total[elem] || 0) + 1; 
+  //return total; }, {});
   
   Object.entries(objArray).sort((e1, e2) => {
     const res = e2[1] - e1[1];
     return res === 0 ? e1[0].localeCompare(e2[0]) :  res;
   }).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
   }
-//test
-const arNames = ['john', 'mary', 'sid', 'john', 'tom', 'mary', 'tom', 'john'];
+
+function objOccurances(ar){
+  return ar.reduce((total, elem) => {total[elem] = (total[elem] || 0) + 1; 
+    return total; }, {});
+}
+
+//hw-15 part 2
+function countBy(ar, callBackFunc){
+   return ar.reduce((tot, el) => {tot[callBackFunc(el)] = (tot[callBackFunc(el)] || 0) + 1; return tot;}, {});
+}
+
+
+  //test
+const arNames = ['john', 'mary', 'sid', 'john', 'li', 'tom', 'mary', 'tom', 'john'];
+
 dispStat(arNames);
+const statArNames = countBy(arNames, element => element.length);
+console.log(statArNames);
+
+//hw-15 part 2
+// write useful function countBy(array, callbackFun) that returns object
+// with keys as grouping criteria  and values as the occurrence counts
+
+//where array - any array, callbackFun - function returning grouping criteria
+//examples:
+// const arr = [6.4, 7.3, 6.5, 6.9];
+//const statistics = countBy(arr, element => Math.floor(element))
+//result: statistics -> {"6": 3, "7":1}
+// const arr = ['abcd', 'lmnr', 'ab', 'dddd'];
+//const statistics = countBy(arr, element => element.length)
+//result: statistics -> {"4": 3, "2":1}
+//const arr = [{age: 25, id: 123, name: 'Vasya'},{age: 50, id: 123, name: 'Vasya'},
+// {age: 25, id: 123, name: 'Vasya'},{age: 70, id: 123, name: 'Vasya'}  ]
+//const statistics = countBy(arr, element -> element.age)
+//result statistics -> {"25":2, "50":1, "70":1}
 
 
+// const testAr = [{age: 25, id: 123, name: 'Vasya'},{age: 50, id: 123, name: 'Vasya'},
+//  {age: 25, id: 123, name: 'Vasya'}, {age: 70, id: 123, name: 'Vasya'} ];
 
+//  const testAr1 = ['qwe', 'qrt', 'qazx', 'as', 'fg', 'jkhg', 'f'];
+ 
