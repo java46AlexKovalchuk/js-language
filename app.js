d84110779210def102623bee3382ef5017ddb730
AlexKovalchuk <kovalchuk111@gmail.com>
2022-01-31 23:43:05 +0200

    js-6-oop

--- app.js
+++ app.js
@@ -1,156 +1,86 @@
+//obj.fun(arguments)
+//1. Encapsulation - data are encapsulated inside an object. Data processing done through the object function
+//2. Inheritance. Any encapsulation may inherit common encapsulation with common data. Example: 
+//  Employee and Child may inherit the encapsulation of Person. It allows avoid repeating the common data.
 
-//  const euros = [29.76, 41.85, 46.5];
-// // const doubled = euros.reduce((total, amount) => {total.push(amount * 2)
-// //     return total;},[])
-// // console.log(doubled);
-// const above30 = euros.reduce((total, amount) => {amount > 30 ? total.push(amount) : undefined
-//  return total }, [])
-// console.log(above30);
+//3. POlymorphism 
+// function(arg){
+//  arg.toString(); -> the method toString will be called in accordence to the arg type. This is polymorphism. 
+//  }
 
-const fruitBasket = ['banana', 'cherry', 'orange', 'apple', 'cherry', 'orange', 'apple', 'banana', 'cherry', 'orange', 'fig' ];
-const count = fruitBasket.reduce((tally, fruit) => { !tally[fruit] ? tally[fruit] = 1 : 
- tally[fruit] += 1 
- return tally} , {})
-// const count = fruitBasket.reduce((tally, fruit) => {
-//     if(!tally[fruit]) {
-//         tally[fruit] = 1;
-//     }
-//     else {
-//         tally[fruit] += 1;
-//     }
-//     //tally[fruit] = (tally[fruit] || 0) + 1 ;
-//   return tally;
-// }, {} );
-console.log(count);
+const circle = {radius: 20, square: function(){
+    return 3.14 * (this.radius ** 2)
+}, perimeter: () => 2 * 3.14 * this.radius,        // in  any arrow function there is no "this" !
+toString: function(){
+  return `radius of this circle is ${this.radius}`
+}};  
+console.log(`square = ${circle.square()}, perimeter = ${circle.perimeter()}`);
+//circle.square wiil be converted to square(circle) where circle as argument value, "this" is the hidden
+//parameter in the function.
+//in  any arrow function there is no "this"
+ function square(circle) {
+   return 3.14 * (circle.radius ** 2);
+ }
+ console.log(`circle: ${circle}`);
+ 
 
-// const data = [
-//     {a: 'happy', b: 'robin', c: ['blue','green']}, 
-//     {a: 'tired', b: 'panther', c: ['green','black','orange','blue']}, 
-//     {a: 'sad', b: 'goldfish', c: ['green','red']}
-//   ];
+ const circle1 =  {radius: 20,  perimeter: function(){
+    return 2 * 3.14 * this.radius}, 
+    toString: function(){
+      return `radius of this circle is ${this.radius}`
+    }}; 
+   // circle1.square(); error because the method square is not defined inside object circle1
 
-// const colors = data.reduce((total, el) => {el.c.forEach(color => {total.push(color)}); 
-// return total}, []); 
-//   console.log(colors);
+   function Circle(radius) {
+       this.radius = radius;
+   }
+   Circle.prototype.square = function() {
+      return 3.14 * (this.radius ** 2);
+   }
+   Circle.prototype.perimeter = function(){
+     return 2 * 3.14 * this.radius;
+   }
+   Circle.prototype.toString = function(){
+     return `radius of this circle is ${this.radius}`
+   }
+  const circle10 = new Circle(10);
+  //********************HW-16: */
+  //write constructor Deffersd
+  // const d = new Deffered()
+// d.then(function(res){ console.log('1 ', res); return 'a'; });
+// d.then(function(res){ console.log('2 ', res); return 'b'; });
+// d.then(function(res){ console.log('3 ', res); return 'c'; });
+// d.resolve('hello');
   
-// const uniqColors = data.reduce((total, el) => {el.c.forEach(color => {
-//   if (total.indexOf(color, ) === -1){
-//     total.push(color);
-//   }
-//   })
-//   return total;
-// }, []);
-// console.log(uniqColors);
-// const arStr1 = ['jhg', 'aer', 'dfg', 'jkl'];
-// const arOrd = arStr1.sort(); 
-// console.log(arOrd);
+  // output:
+  //1 hello
+  //2 a
+  //3 b
 
-// const test = ['hgf', 'fdgfd', 'jhfhggd', 'dsads'];
-// function displayOccurrences(array) {
-//   const res = {};
-//   for (let i = 0; i < array.length; i++) {
-//           if (res[array[i]] === undefined) {
-//                   //string as content of array[i] occures first time
-//                   res[array[i]] = 1;
-//           } else {
-//                   res[array[i]] = res[array[i]] + 1;
-//           }
-//   }
-//   //console.log(res) //-> intermediate debug log
-//   Object.entries(res).sort((e1, e2) => {
-//           const res = e2[1] - e1[1];
-//           return res === 0 ? e1[0].localeCompare(e2[0]) :  res;
-//   }).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
+  //task2
+  // 
+  //const myArray = new myArray(10); 
+  //myArray.get(index)   - result 10
+  //write method get getting an index value and returning common value (set in constructor)
+  //myArray.set(index, value);
+  //write method set that sets a given value and a given value
+  //myArray.setValue(value) - sets new value in all elements of myArray
+ // Example:
+//  const myArray = new MyArray(10);
+//  console.log(myArray(100))  //-> display out 10
+//  myArray.set(100, 500) //sets 500 at index 100
+//  console.log(myArray.get(200)) //displayed out 10
+//  console.log(myArray.get(100)) //displayed out 500
+//  myArray.setValue(300);
+//  console.log(myArray.get(100)) //displayed out 300
+//  console.log(myArray(200)) //displayed out 300
 
-
-// }
-// const ar =  ["bc", "lmn", "d", "d", "lmn", "a", "lmn", "a"];
-// displayOccurrences(ar);
-
-
-
-// function dispStat (ar){ 
-// const objArray = ar.reduce((total, elem) => {total[elem] = (total[elem] || 0) + 1; 
-// return total; }, {});
-
-// Object.entries(objArray).sort((e1, e2) => {
-//   const res = e2[1] - e1[1];
-//   return res === 0 ? e1[0].localeCompare(e2[0]) :  res;
-// }).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
-// }
-
-function dispStat (ar){ 
-  
-  const objArray = objOccurances(ar);
-  //ar.reduce((total, elem) => {total[elem] = (total[elem] || 0) + 1; 
-  //return total; }, {});
-  
-  Object.entries(objArray).sort((e1, e2) => {
-    const res = e2[1] - e1[1];
-    return res === 0 ? e1[0].localeCompare(e2[0]) :  res;
-  }).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
-  }
-
-function objOccurances(ar){
-  return ar.reduce((total, elem) => {total[elem] = (total[elem] || 0) + 1; 
-    return total; }, {});
+//*************
+Array.prototype.filter = function(callbackPredicate){
+    console.log('Tel-Ran copiright');
+    const res = []      
+    this.forEach((n, i, a) => callbackPredicate(n, i, a) && res.push(n));
+    return res;
 }
-
-//hw-15 part 2
-function countBy(ar, callBackFunc){
-   return ar.reduce((tot, el) => {tot[callBackFunc(el)] = (tot[callBackFunc(el)] || 0) + 1; return tot;}, {});
-}
-
-
-  //test
-const arNames = ['john', 'mary', 'sid', 'john', 'li', 'tom', 'mary', 'tom', 'john'];
-
-dispStat(arNames);
-const statArNames = countBy(arNames, element => element.length);
-console.log(statArNames);
-
-//hw-15 part 2
-// write useful function countBy(array, callbackFun) that returns object
-// with keys as grouping criteria  and values as the occurrence counts
-
-//where array - any array, callbackFun - function returning grouping criteria
-//examples:
-// const arr = [6.4, 7.3, 6.5, 6.9];
-//const statistics = countBy(arr, element => Math.floor(element))
-//result: statistics -> {"6": 3, "7":1}
-// const arr = ['abcd', 'lmnr', 'ab', 'dddd'];
-//const statistics = countBy(arr, element => element.length)
-//result: statistics -> {"4": 3, "2":1}
-//const arr = [{age: 25, id: 123, name: 'Vasya'},{age: 50, id: 123, name: 'Vasya'},
-// {age: 25, id: 123, name: 'Vasya'},{age: 70, id: 123, name: 'Vasya'}  ]
-//const statistics = countBy(arr, element -> element.age)
-//result statistics -> {"25":2, "50":1, "70":1}
-
-
-// const testAr = [{age: 25, id: 123, name: 'Vasya'},{age: 50, id: 123, name: 'Vasya'},
-//  {age: 25, id: 123, name: 'Vasya'}, {age: 70, id: 123, name: 'Vasya'} ];
-
-//  const testAr1 = ['qwe', 'qrt', 'qazx', 'as', 'fg', 'jkhg', 'f'];
-
-//test callback
-
-function each (ar, callback){
-  let result = [];
-  for(let elem of ar) {
-  result.push(callback(elem));    //call callback
-  }
-  return result;
-}
-
-const ar1 = ['qwe', 'ert', 'ghj'];  
-const callback = (num => num.split('').reverse().join(''));
-const str2 = each(ar1, callback);
-console.log(str2);
-
-// let arRes = each(ar1, callback);
-// // let arRes = each(ar1, function (num){
-// //   return num ** 2;
-// // });
-
-
-
+const ar = [1, 2, 3, 4, 5, 100]
+ar.filter(n => n % 2 == 0).forEach(n => console.log(n));
