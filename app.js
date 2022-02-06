0b2e27c80039184cd3f3126824737c603353f7a6
AlexKovalchuk <kovalchuk111@gmail.com>
2022-02-06 17:20:54 +0200

    +hw17 part1

--- app.js
+++ app.js
@@ -1,79 +1,94 @@
 
- class Person{
-       #id = id;
-       #name = name;
-       constructor(id, name){
-             this.#id = id;
-             this.#name = name
-       }
-       getId(){
-             return this.#id;
-       }
-       getName(){
-             return this.#name;
-       }
+class Person {
+      #id;
+      #name;
+      constructor(id, name) {
+              this.#id = id;
+              this.#name = name
+      }
+      getId() {
+              return this.#id;
+      }
+      getName() {
+              return this.#name
+      }
+      toString() {
+              return `id: ${this.#id}; name: ${this.#name};`
+      }
 }
-
 const person = new Person(123, 'Moshe');
-console.log(person);
+console.log(`person is ${person}`);
 
-// class Employee extends Person {
-//         #salary;
-//         constructor(id, name, salary){
-//             super(id, name);   //call the constructor of the class Person
-//             this.#salary = salary;
-//         }
-//         computeSalary(){
-//               return this.#salary;
-//         }
-//         toString(){
-//               return super.toString() + ` salary: ${this.computeSalary()}`
-//         }
-// }
-// const person2 = new Employee(124, "Sara", 5000);
+class Employee extends Person {
+      #salary;
+      constructor(id, name,salary){
+           super(id, name)  // call the constructor of the class Person
+           this.#salary = salary;
+      }
+      computeSalary(){
+            return this.#salary;
+      }
+      toString(){
+            return super.toString() + ` salary: ${this.computeSalary()}`;
+      }
+}
+const person2 = new Employee(124, 'Sara', 5000);
+console.log(`person2 is ${person2}`);
+console.log(typeof(person2)); // just object
+console.log(person2.constructor.name);// only this way JS allows getting constructor name
 
-// console.log(`person2 is ${person2}`);
+class Child extends Person {
+      #kindergarten;
+      constructor(id, name, kindergarten){
+            super(id, name);
+            this.#kindergarten = kindergarten;
+      }
+      getKindergarten(){
+            return this.#kindergarten;
+      }
+      toString(){
+            return `${super.toString()} kindergarten ${this.#kindergarten}`; 
+      }
+}
+const person3 = new Child(125, 'Yakob', 'Shalom');
+console.log(`person3 is ${person3}`);
 
-// console.log(typeof(person2)); //just object
-// console.log(person2.constructor.name); // only this way JS allows getting constructor name
+class WageEmployee extends Employee {
+      #hours;
+      #wage;
+      constructor(id, name, salary, hours, wage){
+            super(id, name, salary)
+            this.#hours = hours;
+            this.#wage = wage;
+      }
+      computeSalary(){
+            return super.computeSalary() + this.#hours * this.#wage;
+      }
+}
+const person4 = new WageEmployee(126, 'Asaf', 1000, 10, 100);
+console.log(`person4 is ${person4}`);
 
-// class Child extends Person {
-//         #kindergarten;
-//         constructor(id, name, kindergarten) {
-//             super(id, name);
-//             this.#kindergarten = kindergarten;
-//         }
-//         getKindergarten(){
-//             return this.#kindergarten;
-//         }
-//         toString() {
-//             return `${super.toString()} kindergarten: ${this.#kindergarten}`
-//         }
 
-// }
-// const person3 = new Child(125, 'Yakob', 'Shalom');
-// console.log(`person3 is ${person3}`);
 
-// class WageEmployee extends Employee {
-//         #hours;
-//         #wage;
-//         constructor(id, name, salary, hours, wage) {
-//                 super(id, name, salary);
-//                 this.#hours = hours;
-//                 this.#wage = wage;
-//         }
-//         computeSalary(){
-//               return super.computeSalary() + this.#hours * this.#wage
-//         }
-//         toString(){
-//               return super.computeSalary() + this.#hours * this.#wage
-//         }
-// }
-// const person4 = new WageEmployee(126, 'Asaf', 1000, 10, 100);
-// console.log(`person4 is ${person4}`)
 
-// //******************* */
-// const person = [
+const persons = [
+  new Child(100, 'Olya', 'Shalom'), 
+  new Child(101, 'Serega', 'Boker'),
+  new Child(102, 'Kolya', 'Shalom'),
+  new Employee(103, 'Vasya', 1000),
+  new WageEmployee(104, 'Tolya', 1000, 10, 100)
+]
+
+const getListTypes = persons.map(n => n.constructor.name);
+console.log(getListTypes);
+
+console.log(`${persons}`);
+
+
+
+
+// //*****************HW-17** */
+// const persons = [
 //   new Child(100, 'Olya', 'Shalom'), 
 //   new Child(101, 'Serega', 'Boker'),
 //   new Child(102, 'Kolya', 'Shalom'),
