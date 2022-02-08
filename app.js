669e9012d67e51667c679620415844596994475d
AlexKovalchuk <kovalchuk111@gmail.com>
2022-02-08 16:22:45 +0200

    commit n

--- app.js
+++ app.js
@@ -1,146 +1,39 @@
-
-class Person {
-      #id;
-      #name;
-      constructor(id, name) {
-              this.#id = id;
-              this.#name = name
-      }
-      getId() {
-              return this.#id;
-      }
-      getName() {
-              return this.#name
-      }
-      toString() {
-              return `id: ${this.#id}; name: ${this.#name};`
-      }
-}
-const person = new Person(123, 'Moshe');
-console.log(`person is ${person}`);
-
-class Employee extends Person {
-      #salary;
-      constructor(id, name,salary){
-           super(id, name)  // call the constructor of the class Person
-           this.#salary = salary;
-      }
-      computeSalary(){
-            return this.#salary;
-      }
-      toString(){
-            return super.toString() + ` salary: ${this.computeSalary()}`;
-      }
-}
-const person2 = new Employee(124, 'Sara', 5000);
-console.log(`person2 is ${person2}`);
-console.log(typeof(person2)); // just object
-console.log(person2.constructor.name);// only this way JS allows getting constructor name
-
-class Child extends Person {
-      #kindergarten;
-      constructor(id, name, kindergarten){
-            super(id, name);
-            this.#kindergarten = kindergarten;
-      }
-      getKindergarten(){
-            return this.#kindergarten;
-      }
-      toString(){
-            return `${super.toString()} kindergarten ${this.#kindergarten}`; 
-      }
-}
-const person3 = new Child(125, 'Yakob', 'Shalom');
-console.log(`person3 is ${person3}`);
-
-class WageEmployee extends Employee {
-      #hours;
-      #wage;
-      constructor(id, name, salary, hours, wage){
-            super(id, name, salary)
-            this.#hours = hours;
-            this.#wage = wage;
-      }
-      computeSalary(){
-            console.log('logging of parameter this' , this);
-            return super.computeSalary() + this.#hours * this.#wage;
-      }
-}
-const person4 = new WageEmployee(126, 'Asaf', 1000, 10, 100);
-console.log(`person4 is ${person4}`);
-
-
-
-
-const persons = [
-  new Child(100, 'Olya', 'Shalom'), 
-  new Child(101, 'Serega', 'Boker'),
-  new Child(102, 'Kolya', 'Shalom'),
-  new Employee(103, 'Vasya', 1000),
-  new WageEmployee(104, 'Tolya', 1000, 10, 100)
-]
-
-function computeSalaryBudget(persons){
-      const allEmployees = persons.filter(p => !!p.computeSalary);
-      const salaryValues = allEmployees.map(p => p.computeSalary());
-      return salaryValues.reduce((res, cur) => res + cur);
-}
-//HW-17
-const getListTypes = persons.map(n => n.constructor.name);
-//console.log(getListTypes);
-const qntTypes = getListTypes.filter(n => n == 'Child');
-//console.log(qntTypes);
-const numberTheType = qntTypes.length;
-console.log(numberTheType);
-
-function countOfPersonType(persons, type){
-    const listTypes = persons.map(n=> n.constructor.name);
-    const qntTypes = listTypes.filter(n => n === type);
-    return qntTypes.length;
-}  
-const numType = countOfPersonType(persons, 'Employee');
-console.log(numType);
-      
-      //   //return count of persons of the given type
-      //   //Example:
-      //   //countOfPersonType(persons, 'WageEmployee')    --->  1
-      // }
-
-
-//console.log(`${persons}`);
-
-
-
-
-// //*****************HW-17** */
-// const persons = [
-//   new Child(100, 'Olya', 'Shalom'), 
-//   new Child(101, 'Serega', 'Boker'),
-//   new Child(102, 'Kolya', 'Shalom'),
-//   new Employee(103, 'Vasya', 1000),
-//   new WageEmployee(104, 'Tolya', 1000, 10, 100)
-// ]
-
-// function countOfPersonType(persons, type){
-//   //return count of persons of the given type
-//   //Example:
-//   //countOfPersonType(persons, 'WageEmployee')    --->  1
-// }
-// function computeSalaryBudget(persons){
-//   //returns total salary of all employee objects in the given array
-//   //Example:
-//   //computeSalaryBuget(persons) ---> 3000;
-//   function howChildrenKindergarten(persons, kindergaerten) {
-//     //returns number of children in the given kindergarten
-//     //example:
-//     //computeChildrenKindergarten(persons ....)
-//   }
-// }
-
-function testOutput(fun, expected){
-   console.log(`function ${fun.name} ; expected result: ${expected} ; actual result: ${fun()} `)   
+const point = {
+      x: 3,
+      y: 4
+};
+function displayPointInSpace(z, t){
+      //"this" - reference to any object having properties x and y
+      console.log(`x: ${this.x}, y: ${this.y}, z: ${z}, t: ${t} `);
 }
 
-//testOutput(person4.computeSalary.bind(person4), 2000);
-//testOutput(computeSalaryBudget.bind(undefined, persons), 3000)
-testOutput(countOfPersonType.bind(undefined, persons, 'Child'), 3);
+// point.method = displayPointInSpace;
+// point.method(10, 20);
+
+// displayPointInSpace.call(point, 10, 20);
+// displayPointInSpace.mybind(point, 10, 20)();
+// displayPointInSpace.apply(point, [10, 20]);
+/*********************************** */
+
+Function.prototype.mybind = function(thisObj, ...args){
+             //this - reference to any functional object (in example - displayPointInSoace)
+             //thisObject - reference to any object(in example - point)
+            
+             return (...params) =>  {
+                  thisObj.method1233456 = this;
+                  const res = thisObj.method1233456(...args.concat(params));
+                  delete thisObj.method1233456;
+                  return res;
+             }
+}
+//arguments are passed at function call
+const funDisplay = displayPointInSpace.mybind(point);
+funDisplay(10, 20); 
+/******************* */
+//all arguments are bound by the method "mybind"
+const funDisplayArguments = displayPointInSpace.mybind(point, 10, 20);
+funDisplayArguments();
+/*************** */
+//mixed - part of arguments are bount by the method mybind and other part of arguments are past at function code
+const funDisplayMixed = displayPointInSpace.mybind(point, 10);
+funDisplayMixed(20)
